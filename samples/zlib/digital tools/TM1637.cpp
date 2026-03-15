8c83dc4142271c702bc9b9012207a9efdad03704
Dnyane <dnyaneshwaripachankar@gmail.com>
2026-03-15 16:23:41 +0530

    init commit

---
+++ samples/zlib/digital tools/TM1637.cpp
@@ -0,0 +1,327 @@
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+//  Modified record:
+//    Author: david.reyna@windriver.com
+//    Date: 12 February, 2016 - add ASCII support, patch for sync timeout, for Rocket/Galileo support
+//
+/*******************************************************************************/
+
+#include "TM1637.h"
+// ...existing code...
+
+void TM1637::display(char *DispData)
+{
+  int8_t SegData[4];
+  uint8_t i;
+  if (!DispData) {
+    for (i = 0; i < 4; i++) SegData[i] = 0x20;
+  } else {
+    for (i = 0; i < 4; i++)
+    {
+      char c = DispData[i];
+      if (c == '\0') SegData[i] = 0x20;
+      else SegData[i] = (int8_t)c;
+    }
+  }
+  coding(SegData);
+  start();
+  writeByte(ADDR_AUTO);
+  stop();
+  start();
+  writeByte(Cmd_SetAddr);
+  for(i = 0; i < 4; i++)
+  {
+    writeByte(SegData[i]);
+  }
+  stop();
+  start();
+  writeByte(Cmd_DispCtrl);
+  stop();
+}
+
+// ...existing code...
+
+int8_t TM1637::coding(int8_t DispData)
+{
+  uint8_t PointData = (_PointFlag == POINT_ON) ? 0x80 : 0;
+  uint8_t data = (uint8_t) DispData;
+
+  if (data == 0x7f || data == 0x20)
+    return 0x00 | PointData; // off/space
+
+  if (data >= '0' && data <= '9')
+    return TubeTab[data - '0'] | PointData;
+
+  if (data >= 'A' && data <= 'F')
+    return TubeTab[10 + data - 'A'] | PointData;
+
+  if (data >= 'a' && data <= 'z') {
+    uint8_t idx = data - 'a';
+    if (idx < sizeof(AsciiTab)/sizeof(AsciiTab[0]))
+      return AsciiTab[idx] | PointData;
+    return 0x00 | PointData;
+  }
+
+  // Support raw digit indices 0..15
+  if (data <= 0x0f)
+    return TubeTab[data] | PointData;
+
+  // fallback blank
+  return 0x00 | PointData;
+}
+
+// ...existing code...
+#ifdef ARDUINO
+#include <Arduino.h>
+#else
+#include <stdint.h>
+#include <stdbool.h>
+
+// Minimal Arduino compatibility layer for non-Arduino builds
+#define OUTPUT 1
+#define INPUT 0
+#define HIGH 1
+#define LOW 0
+typedef uint8_t boolean;
+
+inline void pinMode(uint8_t, uint8_t) {}
+inline void digitalWrite(uint8_t, uint8_t) {}
+inline int digitalRead(uint8_t) { return 0; }
+#endif
+
+static int8_t TubeTab[] = {0x3f,0x06,0x5b,0x4f,
+                           0x66,0x6d,0x7d,0x07,
+                           0x7f,0x6f,0x77,0x7c,
+                           0x39,0x5e,0x79,0x71};//0~9,A,b,C,d,E,F
+
+static int8_t AsciiTab[] = {
+	0x77, // a
+	0x7c, // b
+	0x39, // b
+	0x5e, // d
+	0x79, // e
+	0x71, // f
+	0x6f, // g
+	0x76, // h
+	0x06, // i
+	0x0c, // j
+	0x40, // k
+	0x30, // l
+	0x40, // m
+	0x54, // n
+	0x5c, // o
+	0x73, // p
+	0x67, // q
+	0x50, // r
+	0x6d, // s
+	0x07, // t
+	0x1c, // u
+	0x40, // v
+	0x40, // w
+	0x40, // x
+	0x66, // y
+	0x40  // z
+};
+
+
+TM1637::TM1637(uint8_t Clk, uint8_t Data)
+{
+  Clkpin = Clk;
+  Datapin = Data;
+  pinMode(Clkpin,OUTPUT);
+  pinMode(Datapin,OUTPUT);
+}
+
+void TM1637::init(void)
+{
+  clearDisplay();
+}
+
+void TM1637::writeByte(int8_t wr_data)
+{
+  uint8_t i,count1;
+  for(i=0;i<8;i++)        //sent 8bit data
+  {
+    digitalWrite(Clkpin,LOW);
+    if(wr_data & 0x01)digitalWrite(Datapin,HIGH);//LSB first
+    else digitalWrite(Datapin,LOW);
+    wr_data >>= 1;
+    digitalWrite(Clkpin,HIGH);
+
+  }
+  digitalWrite(Clkpin,LOW); //wait for the ACK
+  digitalWrite(Datapin,HIGH);
+  digitalWrite(Clkpin,HIGH);
+  pinMode(Datapin,INPUT);
+  count1=0;
+  //Serial.print("0");
+  while(digitalRead(Datapin))
+  {
+    count1 +=1;
+
+    if(count1 == 50)
+    {
+      pinMode(Datapin,OUTPUT);
+      digitalWrite(Datapin,LOW);
+    }
+    pinMode(Datapin,INPUT);
+
+	// failover timeout
+    if(count1 == 100)
+    {
+      ackFailCnt++;
+	  break;
+    }
+  }
+  //Serial.println("2");
+  pinMode(Datapin,OUTPUT);
+
+  if (0 < count1) {
+//     Serial.print("=== BAR:");
+//     Serial.println(count1);
+  }
+
+}
+
+//send start signal to TM1637
+void TM1637::start(void)
+{
+  digitalWrite(Clkpin,HIGH);//send start signal to TM1637
+  digitalWrite(Datapin,HIGH);
+  digitalWrite(Datapin,LOW);
+  digitalWrite(Clkpin,LOW);
+}
+//End of transmission
+void TM1637::stop(void)
+{
+  digitalWrite(Clkpin,LOW);
+  digitalWrite(Datapin,LOW);
+  digitalWrite(Clkpin,HIGH);
+  digitalWrite(Datapin,HIGH);
+}
+
+//display function.Write to full-screen.
+void TM1637::display(int8_t DispData[])
+{
+  int8_t SegData[4];
+  uint8_t i;
+  for(i = 0;i < 4;i ++)
+  {
+    SegData[i] = DispData[i];
+  }
+  coding(SegData);
+  start();          //start signal sent to TM1637 from MCU
+  writeByte(ADDR_AUTO);//
+  stop();           //
+  start();          //
+  writeByte(Cmd_SetAddr);//
+  for(i=0;i < 4;i ++)
+  {
+    writeByte(SegData[i]);        //
+  }
+  stop();           //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+
+//display function.Write to full-screen.
+void TM1637::display(char *DispData)
+{
+  int8_t SegData[4];
+  uint8_t i;
+  for(i = 0;i < 4;i ++)
+  {
+    SegData[i] = (int8_t) DispData[i];
+  }
+  coding(SegData);
+  start();          //start signal sent to TM1637 from MCU
+  writeByte(ADDR_AUTO);//
+  stop();           //
+  start();          //
+  writeByte(Cmd_SetAddr);//
+  for(i=0;i < 4;i ++)
+  {
+    writeByte(SegData[i]);        //
+  }
+  stop();           //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+
+//******************************************
+void TM1637::display(uint8_t BitAddr,int8_t DispData)
+{
+  int8_t SegData;
+  SegData = coding(DispData);
+  start();          //start signal sent to TM1637 from MCU
+  writeByte(ADDR_FIXED);//
+  stop();           //
+  start();          //
+  writeByte(BitAddr|0xc0);//
+  writeByte(SegData);//
+  stop();            //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+
+void TM1637::clearDisplay(void)
+{
+  display(0x00,0x7f);
+  display(0x01,0x7f);
+  display(0x02,0x7f);
+  display(0x03,0x7f);
+}
+
+//To take effect the next time it displays.
+void TM1637::set(uint8_t brightness,uint8_t SetData,uint8_t SetAddr)
+{
+  Cmd_SetData = SetData;
+  Cmd_SetAddr = SetAddr;
+  Cmd_DispCtrl = 0x88 + brightness;//Set the brightness and it takes effect the next time it displays.
+}
+
+//Whether to light the clock point ":".
+//To take effect the next time it displays.
+void TM1637::point(boolean PointFlag)
+{
+  _PointFlag = PointFlag;
+}
+
+void TM1637::coding(int8_t DispData[])
+{
+  for(uint8_t i = 0;i < 4;i ++)
+    DispData[i] = coding(DispData[i]);
+}
+
+int8_t TM1637::coding(int8_t DispData)
+{
+  uint8_t PointData;
+  if(_PointFlag == POINT_ON) PointData = 0x80;
+  else PointData = 0;
+
+  if      (DispData == 0x7f)
+  	DispData = 0x00 + PointData;//The bit digital tube off
+  else if (DispData == 0x20)
+  	DispData = 0x00 + PointData;//space char
+  else if ((DispData >= 0x61) && (DispData < 0x7a))
+  	DispData = AsciiTab[DispData-0x61] | PointData;
+  else
+  	DispData = TubeTab[DispData] | PointData;
+
+  return DispData;
+}
